b6cfffa7184ccce48f8ea45ed489cb40a08c3de0
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-05-13 15:21:47 -0700

    @home commit to break for review

--- flask/checkerboard/server.py
+++ flask/checkerboard/server.py
@@ -1,12 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/play/<string:color1>')
+def mk_board(color1):
+    return render_template("index.html", color1=color1)
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
