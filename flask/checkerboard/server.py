0152fde8084e579a1423a56d68373645a1f7215b
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-05-13 19:14:19 -0700

    @home commit gotta finish variable passing

--- flask/checkerboard/server.py
+++ flask/checkerboard/server.py
@@ -6,9 +6,13 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/play/<string:color1>')
-def mk_board(color1):
-    return render_template("index.html", color1=color1)
+@app.route('/cb')
+def mk_board():
+    return render_template("index.html")
+
+# @app.route('/cb/<int:num>')
+# def refactor():
+#     return render_template("index.html", num = num)
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
