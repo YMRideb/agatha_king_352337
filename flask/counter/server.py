b8fef139f94b0c64dcb87056136606f277a060d6
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-05-15 12:00:18 -0700

    @home counter initialize commit2

---
+++ flask/counter/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, session  # added session
+app = Flask(__name__)
+# set a secret key for sevcurity purposes
+app.secret_key = 'keep it secret, keep it safe'
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/users', methods=(['POST']))
+def create_user():
+    print("Got Post Info")
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+
+
+@app.route("/show")
+def show_user():
+    print("Showing the User Info From the Form")
+    print(request.form)
+    return render_template("show.html", name_on_template=session['username'], email_on_template=['useremail'])
+
+
+if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
+    app.run(debug=True)    # Run the app in debug mode.
