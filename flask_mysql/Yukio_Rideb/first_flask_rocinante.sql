cd9e8e7bb90af10a83e949f3d8ddb21cb5e00ef6
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-05-19 07:08:26 -0700

    @home CRUD Begin Commit

---
+++ flask_mysql/Yukio_Rideb/first_flask_rocinante.sql
@@ -0,0 +1,5 @@
+SELECT * FROM first_flask.friends;
+INSERT INTO first_flask.friends (first_name, last_name, occupation)
+VALUES ("James", "Holden", "Captain"), ("Naomi", "Nagata", "Executive Officer"),
+("Alex", "Kamal", "Pilot"),("Amox", "Burton", "Mechanic"), ("Roberta", "Draper", "Gunny SGT");
+SELECT * FROM first_flask.friends;
