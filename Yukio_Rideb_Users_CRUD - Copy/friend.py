d97bef8044e15d6ef588b0e87c4e66affd18f283
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-05-23 11:24:21 -0700

    @home copy directory commit

---
+++ Yukio_Rideb_Users_CRUD - Copy/friend.py
@@ -0,0 +1,56 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the friend table from our database
+
+# can replace friend with user
+class Friend:
+    def __init__(self, data):
+        self.id = data['id']
+        self.name1 = data['name1']
+        self.name2 = data['name2']
+        self.email_address = data['email_address']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    # Now we use class methods to query our database
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM table_crud;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('crud_table').query_db(query)
+        # Create an empty list to append our instances of friends
+        friends = []
+        # Iterate over the db results and create instances of friends with cls.
+        for friend in results:
+            friends.append(cls(friend))
+        return friends
+    
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM table_crud WHERE id = %(id)s;"
+        # data is a dictionary that will be passed into the save method from server.py
+        results = connectToMySQL('crud_table').query_db(query, data)
+        return cls(results[0])
+
+
+    @classmethod
+    def create(cls, data):
+        query = "INSERT INTO table_crud ( name1 , name2 , email_address , created_at, updated_at ) VALUES ( %(name1)s , %(name2)s , %(email_address)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('crud_table').query_db(query, data)
+
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE table_crud SET name1= %(name1)s, name2 = %(name2)s, email_address = %(email_address)s, created_at = NOW(), updated_at = NOW() WHERE id = %(id)s;"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('crud_table').query_db(query, data)
+
+    @classmethod
+    def delete(cls, data):
+        # query = "SET SQL_SAFE_UPDATES = 0"
+        query = "DELETE FROM table_crud WHERE id = %(id)s; "
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('crud_table').query_db(query, data)
+    
+    
---
+++ Yukio_Rideb_Users_CRUD - Copy/project_app/server.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect
+# import the class from friend.py
+from friend import Friend
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    friends = Friend.get_all()
+    # print(friends)
+    return render_template("index.html", friends=friends)
+
+@app.route("/test")
+def test_route():
+        friends = Friend.get_all()
+        return render_template("test.html", friends=friends)
+        
+# relevant code snippet from server.py
+
+
+@app.route('/create_friend', methods=["POST"])
+def create_friend():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    Friend.save(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect('/')
+
+
+@app.route("/edit_user_by_id")
+def edit_user():
+    friends = Friend.get_all()
+    return render_template("/edit_user_by_id.html", friends=friends)
+
+
+@app.route("/show_user_by_id")
+def show_user():
+    friends = Friend.get_all()
+    return render_template("/show_user_by_id.html", friends=friends)
+
+if __name__ == "__main__":
+    app.run(debug=True)
