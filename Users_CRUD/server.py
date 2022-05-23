cc9887c66d77971c95412b0a3da9589aa54a8808
YMRideb <103914884+YMRideb@users.noreply.github.com>
2022-05-23 07:48:01 -0700

    #home CRUD Submission

--- Users_CRUD/server.py
+++ Users_CRUD/server.py
@@ -18,21 +18,63 @@ def test_route():
         
 # relevant code snippet from server.py
 
+@app.route("/users/<int:id>")
+def show_user(id):
+    data = {
+        "id" : id
+    }
+    friends = Friend.get_one(data)
+    return render_template("users.html", friends=friends)
+
+
+@app.route("/users/edit/<int:id>")
+def edit_user(id):
+    data = {
+        "id": id
+    }
+    friends = Friend.get_one(data)
+    return render_template("edit_user.html", friends=friends)
+
 
 @app.route('/create_friend', methods=["POST"])
 def create_friend():
     # First we make a data dictionary from our request.form coming from our template.
     # The keys in data need to line up exactly with the variables in our query string.
     data = {
-        "fname": request.form["fname"],
-        "lname": request.form["lname"],
-        "occ": request.form["occ"]
+        "name1": request.form["name1"],
+        "name2": request.form["name2"],
+        "email_address": request.form["email_address"]
     }
     # We pass the data dictionary into the save method from the Friend class.
-    Friend.save(data)
+    Friend.create(data)
     # Don't forget to redirect after saving to the database.
     return redirect('/')
 
 
+@app.route('/user/update', methods=["POST"])
+def user_update():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "name1": request.form["name1"],
+        "name2": request.form["name2"],
+        "email_address": request.form["email_address"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    Friend.update(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect('/users')
+
+
+@app.route("/users/delete/<int:id>")
+def delete_user(id):
+    data = {
+        "id": id
+    }
+    Friend.delete(data)
+    return redirect('/')
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+# Friend.update(request.form)
